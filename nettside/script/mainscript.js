fe737ebd1d1a2f6b514432b901180a2a028fb0cc
Anneline <anneline.guttormsen@hotmail.no>
2019-05-26 15:11:49 +0200

    26.05

--- nettside/script/mainscript.js
+++ nettside/script/mainscript.js
@@ -1,25 +1,57 @@
-//lag XHR basert på om det er i IE eller ikke
-if (window.XMLHttpRequest) {
-    var xhr = new XMLHttpRequest();
-} else {
-    //hvis IE bruk activex
-    var xhr = new ActiveXObject("Microsoft.XMLHTTP");
-}
+var archiveDiv = document.getElementsByClassName("archive-info")[0];
 
-//url er bare et eksempel, sett opp data til global variabel
-var url = "https://api.spacexdata.com/v3/launches";
+var baseUrl = "https://api.spacexdata.com/v3/";
 var data;
 
-xhr.onreadystatechange = function(){
-    if (this.readyState == 4) {
-        if(this.status == 200) {
-            data = JSON.parse(xhr.responseText);
-            //eventuell funksjon som skal bruke data
-        }
-        else {
-            console.log("Fil ikke funnet 404");
-        }
-    }
+function getArchive(urlArg) {
+  //lag XHR basert på om det er i IE eller ikke
+  if (window.XMLHttpRequest) {
+      var xhr = new XMLHttpRequest();
+  } else {
+      //hvis IE bruk activex
+      var xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  var url = (baseUrl + urlArg);
+  xhr.onreadystatechange = function(){
+      if (this.readyState == 4) {
+          if(this.status == 200) {
+              data = JSON.parse(xhr.responseText);
+              //her går eventuelle funksjoner
+              if (urlArg == "history") {
+                implementHistory();
+              }
+              else if (urlArg == "rockets") {
+                implementRockets();
+              }
+              else if (urlArg == "missions") {
+                implementMissions();
+              }
+              else if (urlArg == "launches") {
+                implementLaunches();
+              }
+          }
+          else {
+              console.log("Fil ikke funnet 404");
+          }
+      }
+  }
+  xhr.open("GET",url,true);
+  xhr.send();
 }
-xhr.open("GET",url,true);
-xhr.send();
+
+function implementHistory() {
+  /*archiveDiv.innerHTML = "";
+  for (var i in data) {
+    archiveDiv.innerHTML
+  }*/
+  console.log("history");
+}
+function implementRockets() {
+  console.log("rockets");
+}
+function implementMissions() {
+  console.log("missions");
+}
+function implementLaunches() {
+  console.log("launches");
+}
