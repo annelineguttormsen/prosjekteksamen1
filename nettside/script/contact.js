6c655cfa03bfb7fa960cc80074f771a082af5806
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2019-05-28 10:20:32 +0200

    Fiks knapper
    Både landing page og contact page

--- nettside/script/contact.js
+++ nettside/script/contact.js
@@ -1,5 +1,6 @@
 var submitButton = document.getElementById("submitButton");
 var errorElement = document.getElementById("error");
+var formElement = document.getElementsByTagName("form")[0];
 
 var name = document.getElementById("formName");
 var email = document.getElementById("formEmail");
@@ -20,7 +21,7 @@ function checkInput() {
   checkRegex(phoneRX,phone,"Form must include a valid phone number");
   checkRegex(emptyInput,text,"Form must include message")
   if (errorElement.innerHTML == "") {
-    errorElement.innerHTML = "<p style='color:#fff'>Thanks for your message! We'll get back to you soon.</p>";
+    formElement.innerHTML = "<p style='color:#fff'>Thanks for your message! We'll get back to you soon.</p>"
   }
 }
 function checkRegex(regex,element,errorMessage) {
