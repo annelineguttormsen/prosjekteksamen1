c3bbc569c72fa16cb97a712852e2852a3db1f801
Anneline <anneline.guttormsen@hotmail.no>
2019-06-02 22:08:09 +0200

    Launchtimer funksjon, endret CSS

--- nettside/script/landingpage.js
+++ nettside/script/landingpage.js
@@ -1,4 +1,7 @@
 var launchCounter = document.getElementsByClassName("launch-counter")[0];
+var launchDate;
+var today;
+var launchDifference;
 var data;
 
   if (window.XMLHttpRequest) {
@@ -12,8 +15,8 @@ var data;
       if (this.readyState == 4) {
           if(this.status == 200) {
               data = JSON.parse(xhr.responseText);
-              //her går eventuelle funksjoner
-              implementLaunchTime();
+              var update = setInterval(implementLaunchTime,500);
+              console.log("jeg har funnet " + data);
           }
           else {
               console.log("Fil ikke funnet 404");
@@ -24,5 +27,12 @@ var data;
   xhr.send();
 
 function implementLaunchTime() {
-
+  today = new Date().getTime();
+  launchDate = new Date(data.launch_date_local);
+  launchDifference = launchDate - today;
+  var days = Math.floor(launchDifference/(1000*60*60*24));
+  var hours = Math.floor((launchDifference%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+  var minutes = Math.floor((launchDifference%(1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((launchDifference%(1000 * 60)) / 1000);
+  launchCounter.innerHTML = "<h1>" + days + "d: " + hours + "h: " + minutes + "m: " + seconds + "s</h1>";
 }
