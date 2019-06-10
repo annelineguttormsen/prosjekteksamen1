025ee32c82439631a93bf312523b35a85bd4b5e3
Anneline Guttormsen <anneline.guttormsen@hotmail.no>
2019-06-10 02:51:27 +0200

    Blæ

--- nettside/script/archive.js
+++ nettside/script/archive.js
@@ -32,6 +32,8 @@ function getArchive(urlArg) {
               else if (urlArg == "launches") {
                 implementLaunches();
               }
+              var selectP = selectEl.getElementsByTagName("p")[0];
+              selectP.innerHTML = (urlArg.charAt(0).toUpperCase() + urlArg.slice(1)) + "<img src=\"media/arrow-right-16.svg\">";
           }
           else {
               console.log("Fil ikke funnet 404");
@@ -45,67 +47,69 @@ function getArchive(urlArg) {
 function implementHistory() {
   archiveDiv.innerHTML = "";
   for (var i in data) {
-    var archiveRow = document.createElement("div");
-    archiveRow.setAttribute("class","archive-row");
-    makeElement("h1",data[i].title,archiveRow);
-    makeElement("p",data[i].details,archiveRow);
-    makeElement("a","Article",archiveRow,"href",data[i].links.article);
-    archiveRow.setAttribute("class","archive-row");
-    archiveDiv.appendChild(archiveRow);
+    makeElement("h1",data[i].title);
+    makeElement("p",data[i].details);
+    makeElement("a","Article","href",data[i].links.article);
   }
 }
 function implementRockets() {
   archiveDiv.innerHTML = "";
   for (var i in data) {
-    var archiveRow = document.createElement("div");
-    archiveRow.setAttribute("class","archive-row");
-    makeElement("h1",data[i].rocket_name,archiveRow);
-    makeElement("p",data[i].description,archiveRow);
-    makeElement("img",null,archiveRow,"src",data[i].flickr_images[0]);
-    archiveDiv.appendChild(archiveRow);
+    makeElement("h1",data[i].rocket_name);
+    makeElement("p",data[i].description);
+    makeElement("img",null,"src",data[i].flickr_images[0]);
   }
 }
 function implementMissions() {
   archiveDiv.innerHTML = "";
   for (var i in data) {
-    var archiveRow = document.createElement("div");
-    archiveRow.setAttribute("class","archive-row");
-    makeElement("h1",data[i].mission_name,archiveRow);
-    makeElement("p",data[i].description,archiveRow);
-    archiveDiv.appendChild(archiveRow);
+    makeElement("h1",data[i].mission_name);
+    makeElement("p",data[i].description);
   }
 }
 function implementLaunches() {
   archiveDiv.innerHTML = "";
   for (var i in data) {
-    var archiveRow = document.createElement("div");
-    archiveRow.setAttribute("class","archive-row");
-    makeElement("h1",data[i].mission_name,archiveRow);
+    makeElement("h1",data[i].mission_name);
     if(data[i].links.mission_patch_small !== null) {
-      makeElement("img",null,archiveRow,"src",data[i].links.mission_patch_small);
+      makeElement("img",null,"src",data[i].links.mission_patch_small);
     }
-    makeElement("p",("<b>Rocket: </b>" + data[i].rocket.rocket_name),archiveRow);
-    makeElement("p",("<b>Launch site: </b>" + data[i].launch_site.site_name_long),archiveRow);
+    makeElement("p",("<b>Rocket: </b>" + data[i].rocket.rocket_name));
+    makeElement("p",("<b>Launch site: </b>" + data[i].launch_site.site_name_long));
     if (data[i].details !== null) {
-      makeElement("p",("<b>Details: </b>" + data[i].details),archiveRow);
+      makeElement("p",("<b>Details: </b>" + data[i].details));
     } else {
-      makeElement("p","<b>Details</b>: No details available", archiveRow);
+      makeElement("p","<b>Details</b>: No details available");
     }
-    makeElement("p",("<b>Launch year: </b>" + data[i].launch_year), archiveRow);
+    makeElement("p",("<b>Launch year: </b>" + data[i].launch_year));
     if (data[i].links.article_link !== null) {
-      makeElement("a",(data[i].mission_name + " launch article"),archiveRow,"href",data[i].links.article_link);
+      makeElement("a",(data[i].mission_name + " launch article"),"href",data[i].links.article_link);
     }
-    archiveDiv.appendChild(archiveRow);
   }
 }
 
-function makeElement(elementType, content, parentElement, attr, attrContent) {
+function makeElement(elementType, content, attr, attrContent) {
   var element = document.createElement(elementType);
   if (attr !== undefined && attrContent !== undefined) {
     element.setAttribute(attr,attrContent);
   }
+  var archiveRow = document.createElement("div");
+  archiveRow.setAttribute("class","archive-row");
   element.innerHTML = content;
-  parentElement.appendChild(element);
+  archiveRow.appendChild(element);
+  archiveDiv.appendChild(archiveRow);
 }
 
 getArchive("launches");
+
+selectEl.onfocus = function popUpOptions() {
+  optionsEl.style.display = "block";
+}
+selectEl.onblur = function() {
+  optionsEl.style.display = "none";
+}
+
+window.onresize = function() {
+  optionsEl.style.display = "none";
+  console.log("fjern options");
+}
